7e2fb8e6c9780ea7701ff5f9f0323cf9f733573e
Mohsen Mirhoseini Argi <msn.mirhoseini@gmail.com>
2016-10-24 16:45:48 +0330

    Added jUnit Test to core-lib

---
+++ core-lib/src/test/java/com/mirhoseini/quandoo/domain/interactor/CustomersInteractorTest.java
@@ -0,0 +1,65 @@
+package com.mirhoseini.quandoo.domain.interactor;
+
+import com.mirhoseini.quandoo.domain.client.QuandooApi;
+import com.mirhoseini.quandoo.domain.model.CustomerResponse;
+import com.mirhoseini.quandoo.util.SchedulerProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import rx.Observable;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Mohsen on 24/10/2016.
+ */
+
+public class CustomersInteractorTest {
+
+    CustomersInteractor interactor;
+    QuandooApi api;
+    SchedulerProvider scheduler;
+    CustomerResponse[] expectedResult;
+
+    @Before
+    public void setup() {
+        api = mock(QuandooApi.class);
+        scheduler = mock(SchedulerProvider.class);
+
+        // Set up the stub we want to return in the mock
+        expectedResult = new CustomerResponse[1];
+        expectedResult[0] =  new CustomerResponse(1, "Mohsen", "Mirhoseini Argi");
+
+        // mock scheduler to run immediately
+        when(scheduler.mainThread())
+                .thenReturn(Schedulers.immediate());
+        when(scheduler.backgroundThread())
+                .thenReturn(Schedulers.immediate());
+
+        // mock api result with expected result
+        when(api.getCustomers())
+                .thenReturn(Observable.just(expectedResult));
+
+        // create a real new interactor using mocked api and scheduler
+        interactor = new CustomersInteractorImpl(api, scheduler);
+    }
+
+    @Test
+    public void testLoadCustomers() throws Exception {
+        TestSubscriber<CustomerResponse[]> testSubscriber = new TestSubscriber<>();
+
+        // call interactor with some random params
+        interactor.loadCustomers()
+                .subscribe(testSubscriber);
+
+        // it must return the expectedResult with no error
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertReceivedOnNext(Collections.singletonList(expectedResult));
+    }
+}
